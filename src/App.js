6e6c3b5258164e056107f29f35165261dc1d4813
Kerminate <kepeilin@outlook.com>
2018-04-28 18:07:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import PCIndex from './js/components/pc_index'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <PCIndex />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/js/components/pc_index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PCHeader from './pc_header'
+
+export default class PCIndex extends Component {
+  render () {
+    return (
+      <div>
+        <PCHeader />
+      </div>
+    )
+  }
+}
