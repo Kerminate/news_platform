bad54ceff3e8dd508eed1d93911b0e8cecc9d18d
Kerminate <kepeilin@outlook.com>
2018-04-29 22:08:48 +0800

    feat: add login

--- src/js/components/mobile_header.js
+++ src/js/components/mobile_header.js
@@ -42,20 +42,37 @@ class MobileHeaderInit extends Component {
       method: 'GET'
     }
     const formData = this.props.form.getFieldsValue()
+    console.log(formData)
 
     /* eslint-disable no-undef */
-    fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=' + formData.r_userName + '&r_password=' + formData.r_password + '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
+    fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=' + this.state.action +
+      '&username=' + formData.userName +
+      '&password=' + formData.password +
+      '&r_userName=' + formData.r_userName +
+      '&r_password=' + formData.r_password +
+      '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
       .then(response => response.json())
       .then((json) => {
         this.setState({ userNickName: json.NickuserName, userid: json.UserId })
       })
 
+    if (this.state.action === 'login') {
+      this.setState({ hasLogined: true })
+    }
     message.success('请求成功！')
     this.setModalVisible(false)
   }
 
   login () {
     this.setModalVisible(true)
+  }
+
+  callback (key) {
+    if (key === 1) {
+      this.setState({ action: 'login' })
+    } else if (key === 2) {
+      this.setState({ action: 'register' })
+    }
   }
 
   render () {
@@ -74,17 +91,48 @@ class MobileHeaderInit extends Component {
           {userShow}
         </header>
         <Modal title='用户中心' wrapClassName='vertical-center-modal' visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText='关闭'>
-          <Tabs type='card'>
+          <Tabs type='card' onChange={this.callback.bind(this)}>
+            <TabPane tab='登录' key='1'>
+              <Form onSubmit={this.handleSubmit.bind(this)}>
+                <FormItem label='账户'>
+                  {getFieldDecorator('userName', {
+                    rules: [{ required: true, message: 'Please input sth.' }]
+                  })(
+                    <Input placeholder='请输入您的账号' />
+                  )}
+                </FormItem>
+                <FormItem label='密码'>
+                  {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input sth.' }]
+                  })(
+                    <Input type='password' placeholder='请输入您的密码' />
+                  )}
+                </FormItem>
+                <Button type='primary' htmlType='submit'>登录</Button>
+              </Form>
+            </TabPane>
             <TabPane tab='注册' key='2'>
               <Form onSubmit={this.handleSubmit.bind(this)}>
                 <FormItem label='账户'>
-                  <Input placeholder='请输入您的账号' {...getFieldDecorator('r_username')} />
+                  {getFieldDecorator('r_userName', {
+                    rules: [{ required: true, message: 'Please input sth.' }]
+                  })(
+                    <Input placeholder='请输入您的账号' />
+                  )}
                 </FormItem>
                 <FormItem label='密码'>
-                  <Input type='password' placeholder='请输入您的密码' {...getFieldDecorator('r_password')} />
+                  {getFieldDecorator('r_password', {
+                    rules: [{ required: true, message: 'Please input sth.' }]
+                  })(
+                    <Input type='password' placeholder='请输入您的密码' />
+                  )}
                 </FormItem>
                 <FormItem label='确认密码'>
-                  <Input type='password' placeholder='请再次输入您的密码' {...getFieldDecorator('r_confirmPassword  ')} />
+                  {getFieldDecorator('r_confirmPassword', {
+                    rules: [{ required: true, message: 'Please input sth.' }]
+                  })(
+                    <Input type='password' placeholder='请再次输入您的密码' />
+                  )}
                 </FormItem>
                 <Button type='primary' htmlType='submit'>注册</Button>
               </Form>
--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -22,6 +22,13 @@ class PCHeaderInit extends Component {
     }
   }
 
+  componentWillMount () {
+    if (window.localStorage.userid !== '') {
+      this.setState({ hasLogined: true })
+      this.setState({ userNickName: window.localStorage.userNickName, userid: window.localStorage.userid })
+    }
+  }
+
   setModalVisible (value) {
     this.setState({modalVisible: value})
   }
@@ -42,16 +49,41 @@ class PCHeaderInit extends Component {
       method: 'GET'
     }
     const formData = this.props.form.getFieldsValue()
+    console.log(formData)
 
     /* eslint-disable no-undef */
-    fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=' + formData.r_userName + '&r_password=' + formData.r_password + '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
+    fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=' + this.state.action +
+      '&username=' + formData.userName +
+      '&password=' + formData.password +
+      '&r_userName=' + formData.r_userName +
+      '&r_password=' + formData.r_password +
+      '&r_confirmPassword=' + formData.r_confirmPassword, myFetchOptions)
       .then(response => response.json())
       .then((json) => {
-        this.setState({userNickName: json.NickuserName, userid: json.UserId})
+        this.setState({ userNickName: json.NickUserName, userid: json.UserId })
+        localStorage.userid = json.UserId
+        localStorage.userNickName = json.NickUserName
       })
 
+    if (this.state.action === 'login') {
+      this.setState({ hasLogined: true })
+    }
     message.success('请求成功！')
     this.setModalVisible(false)
+  }
+
+  callback (key) {
+    if (key === 1) {
+      this.setState({ action: 'login' })
+    } else if (key === 2) {
+      this.setState({ action: 'register' })
+    }
+  }
+
+  logout () {
+    window.localStorage.userid = ''
+    window.localStorage.userNickName = ''
+    this.setState({ hasLogined: false })
   }
 
   render () {
@@ -60,11 +92,11 @@ class PCHeaderInit extends Component {
       ? <Menu.Item key='logout' className='register'>
         <Button type='primary'>{this.state.userNickName}</Button>
           &nbsp;&nbsp;
-        <Link target='_blank'>
+        <Link to='/'>
           <Button type='dashed'>个人中心</Button>
         </Link>
           &nbsp;&nbsp;
-        <Button type='ghost'>退出</Button>
+        <Button type='ghost' onClick={this.logout.bind(this)}>退出</Button>
       </Menu.Item>
       : <Menu.Item key='register' className='register'>
         <Icon type='appstore' />注册/登录
@@ -108,17 +140,48 @@ class PCHeaderInit extends Component {
               {userShow}
             </Menu>
             <Modal title='用户中心' wrapClassName='vertical-center-modal' visible={this.state.modalVisible} onCancel={() => this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText='关闭'>
-              <Tabs type='card'>
+              <Tabs type='card' onChange={this.callback.bind(this)}>
+                <TabPane tab='登录' key='1'>
+                  <Form onSubmit={this.handleSubmit.bind(this)}>
+                    <FormItem label='账户'>
+                      {getFieldDecorator('userName', {
+                        rules: [{ required: true, message: 'Please input sth.' }]
+                      })(
+                        <Input placeholder='请输入您的账号' />
+                      )}
+                    </FormItem>
+                    <FormItem label='密码'>
+                      {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input sth.' }]
+                      })(
+                        <Input type='password' placeholder='请输入您的密码' />
+                      )}
+                    </FormItem>
+                    <Button type='primary' htmlType='submit'>登录</Button>
+                  </Form>
+                </TabPane>
                 <TabPane tab='注册' key='2'>
                   <Form onSubmit={this.handleSubmit.bind(this)}>
                     <FormItem label='账户'>
-                      <Input placeholder='请输入您的账号' {...getFieldDecorator('r_username')} />
+                      {getFieldDecorator('r_userName', {
+                        rules: [{ required: true, message: 'Please input sth.' }]
+                      })(
+                        <Input placeholder='请输入您的账号' />
+                      )}
                     </FormItem>
                     <FormItem label='密码'>
-                      <Input type='password' placeholder='请输入您的密码' {...getFieldDecorator('r_password')} />
+                      {getFieldDecorator('r_password', {
+                        rules: [{ required: true, message: 'Please input sth.' }]
+                      })(
+                        <Input type='password' placeholder='请输入您的密码' />
+                      )}
                     </FormItem>
                     <FormItem label='确认密码'>
-                      <Input type='password' placeholder='请再次输入您的密码' {...getFieldDecorator('r_confirmPassword  ')} />
+                      {getFieldDecorator('r_confirmPassword', {
+                        rules: [{ required: true, message: 'Please input sth.' }]
+                      })(
+                        <Input type='password' placeholder='请再次输入您的密码' />
+                      )}
                     </FormItem>
                     <Button type='primary' htmlType='submit'>注册</Button>
                   </Form>
